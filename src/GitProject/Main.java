e8004680731849661431b351c62af68049d99ddd
darkmatterpropulsion <114185424+darkmatterpropulsion@users.noreply.github.com>
2022-11-08 09:46:02 +0100

    Ho aggiunto un ulteriore istanza di esempio

--- src/GitProject/Main.java
+++ src/GitProject/Main.java
@@ -2,4 +2,5 @@ package GitProject;
 
 public class Main {
     GitProject gitProject = new GitProject("Esempio","Questo è un file di esempio");
+    GitProject gitProject2 = new GitProject("Altro esempio","Questo è un ulteriore esempio di funzionamento di github");
 }
